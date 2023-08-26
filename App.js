26d36b48d691bc2219113fa44afa73f21caaba68
duckyfuz <kenf@iMac&cheese.local>
2023-08-26 13:03:57 +0800

    Add button, Expo Audio

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { Button, PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
+        <Button
+          icon="microphone"
+          mode="elevated"
+          onPress={() => console.log("Pressed")}
+        >
+          Start Recording
+        </Button>
       </View>
     </PaperProvider>
   );
