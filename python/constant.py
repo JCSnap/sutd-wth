fedb159feb67c055920cf8dbba8ac3900b8680eb
JCSnap <e0970167@u.nus.edu>
2023-08-26 17:03:32 +0800

    added auto audio detection and loop

---
+++ python/constant.py
@@ -0,0 +1,9 @@
+SYSTEM_MESSAGE = """
+You are a verbal specialist. You will be given a sentence and you are tasked to categorize the sentence into one of the following categories based on emotions:
+: anger, disgust, fear, joy, sadness, surprise, and neutral.
+Just respond with the emotion without any punctuation or capitalization. or explanation.
+"""
+
+THRESH = 1000 
+
+MAX_SILENCE = 40
--- python/main.py
+++ python/main.py
@@ -4,6 +4,11 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 
+# For record audio based on silence
+import pyaudio
+import wave
+import audioop
+
 # For audio to text conversion
 import speech_recognition as sr
 from pydub import AudioSegment
@@ -12,6 +17,10 @@ from pydub import AudioSegment
 import os
 import openai
 import constant
+
+# For text to speech
+from gtts import gTTS
+import pygame
 
 def record_audio_to_wav(filename):
     fs = 44100  # Sample rate
@@ -28,6 +37,65 @@ def record_audio_to_wav(filename):
     write(wav_filename, fs, myrecording)  # Save as WAV file
 
     print(f"Saved recording as {wav_filename}")
+
+def record_voice(thresh=constant.THRESH, max_silence=constant.MAX_SILENCE, filename="voice.wav"):
+    # Initialize pyaudio
+    p = pyaudio.PyAudio()
+
+    # Start the stream
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=44100,
+                    input=True,
+                    frames_per_buffer=1024)
+
+    # Parameters
+    frame_count = 0
+    frames = []
+    is_recording = False
+
+    print("Waiting for a noise to start recording...")
+
+    while True:
+        # Read a chunk from audio input
+        chunk = stream.read(1024)
+
+        # Check for voice
+        rms = audioop.rms(chunk, 2)
+        
+        if rms > thresh:
+            if not is_recording:
+                print("Noise detected, starting recording!")
+                is_recording = True
+            print("Detected voice!")
+            frame_count = 0
+        else:
+            if is_recording:
+                print("No voice detected.")
+                frame_count += 1
+
+        # If recording, append the frames
+        if is_recording:
+            frames.append(chunk)
+
+        # Check for maximum silence
+        if frame_count > max_silence and is_recording:
+            print("Max silence reached, stopping.")
+            break
+
+    # Close and terminate everything properly
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    # Save audio to file
+    if frames:
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(1)
+        wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(44100)
+        wf.writeframes(b''.join(frames))
+        wf.close()
 
 def audio_to_text(audio_file_path):
     recognizer = sr.Recognizer()
@@ -60,12 +128,25 @@ def textToEmotion(text):
     print(emotion)
     return emotion
 
-if __name__ == "__main__":
-    filename = "test"
-    record_audio_to_wav(filename)
+def play_text_as_audio(text, language='en'):
+    tts = gTTS(text=text, lang=language)
+    tts.save("temp.mp3")
 
-    audio_file_path = filename + ".wav"
-    text = audio_to_text(audio_file_path)
-    print(f"Transcribed text: {text}")
-    emotion = textToEmotion(text)
-    print(f"Emotion: {emotion}")
+    pygame.mixer.init()
+    pygame.mixer.music.load("temp.mp3")
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+if __name__ == "__main__":
+    while True:
+        filename = "test"
+        record_voice()
+
+        audio_file_path =  "voice.wav"
+        text = audio_to_text(audio_file_path)
+        print(f"Transcribed text: {text}")
+        emotion = textToEmotion(text)
+        print(f"Emotion: {emotion}")
+        play_text_as_audio(emotion)
