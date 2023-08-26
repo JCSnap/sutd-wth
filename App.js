fe4323f50cf2ddca4b2cd9e9d3d08c629db6b138
duckyfuz <kenf@iMac&cheese.local>
2023-08-26 13:15:36 +0800

    Restructure Home

--- App.js
+++ App.js
@@ -1,54 +1,15 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, PaperProvider } from "react-native-paper";
-import { Audio } from "expo-av";
+import { StyleSheet, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import Home from "./src/pages/Home";
 
 export default function App() {
-  const [recording, setRecording] = React.useState();
-
-  async function startRecording() {
-    try {
-      console.log("Requesting permissions..");
-      await Audio.requestPermissionsAsync();
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      });
-
-      console.log("Starting recording..");
-      const { recording } = await Audio.Recording.createAsync(
-        Audio.RecordingOptionsPresets.HIGH_QUALITY
-      );
-      setRecording(recording);
-      console.log("Recording started");
-    } catch (err) {
-      console.error("Failed to start recording", err);
-    }
-  }
-
-  async function stopRecording() {
-    console.log("Stopping recording..");
-    setRecording(undefined);
-    await recording.stopAndUnloadAsync();
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-    });
-    const uri = recording.getURI();
-    console.log("Recording stopped and stored at", uri);
-  }
-
   return (
     <PaperProvider>
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <Button
-          icon="microphone"
-          mode="elevated"
-          onPress={recording ? stopRecording : startRecording}
-        >
-          {recording ? "Stop Recording" : "Start Recording"}
-        </Button>
+        <Home />
       </View>
     </PaperProvider>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,49 @@
-// This is the home page
+import React from "react";
+import { Button } from "react-native-paper";
+import { Audio } from "expo-av";
+
+const Home = () => {
+  const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log("Requesting permissions..");
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording.getURI();
+    console.log("Recording stopped and stored at", uri);
+  }
+  return (
+    <Button
+      icon="microphone"
+      mode="elevated"
+      onPress={recording ? stopRecording : startRecording}
+    >
+      {recording ? "Stop Recording" : "Start Recording"}
+    </Button>
+  );
+};
+
+export default Home;
