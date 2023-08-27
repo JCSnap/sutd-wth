a4f3df8ec391c8828f0d8dac2a7f99f3e17b4691
JCSnap <e0970167@u.nus.edu>
2023-08-27 12:45:00 +0800

    added constant

--- python/constant.py
+++ python/constant.py
@@ -1,4 +1,16 @@
-SYSTEM_MESSAGE = """
+SYSTEM_MESSAGE1 = """
+You are a verbal specialist. You will be given a sentence and you are tasked to categorize the sentence into one of the following categories based on emotions:
+: anger, disgust, fear, joy, sadness, surprise, and neutral.
+Just respond with the emotion without any punctuation or capitalization. or explanation.
+"""
+
+SYSTEM_MESSAGE2 = """
+You are a verbal specialist. You will be given a sentence and you are tasked to categorize the sentence into one of the following categories based on emotions:
+: mad, distant, disappointed, repulsed, hurt, despair, optimistic, playful, startled, excited, bored, stressed, scared, and insecure.
+Just respond with the emotion without any punctuation or capitalization or explanation.
+"""
+
+SYSTEM_MESSAGE3 = """
 You are a verbal specialist. You will be given a sentence and you are tasked to categorize the sentence into one of the following categories based on emotions:
 : anger, disgust, fear, joy, sadness, surprise, and neutral.
 Just respond with the emotion without any punctuation or capitalization. or explanation.
